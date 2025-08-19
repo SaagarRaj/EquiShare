1de1c723c5679651a72c8eb11cb9025710dff757
SaagarRaj <saagartiwari1992@gmail.com>
2025-08-19 11:57:51 -0700

    Database schema complete

--- convex/schema.js
+++ convex/schema.js
@@ -12,4 +12,52 @@ export default defineSchema({
     .index("by_email", ["email"])
     .searchIndex("search_name", { searchField: "name" })
     .searchIndex("search_email", { searchField: "email" }),
+
+  expenses: defineTable({
+    description: v.string(),
+    amount: v.number(),
+    category: v.optional(v.string()),
+    date: v.number(),
+    paidByUserId: v.id("users"),
+    splitType: v.string(),
+    splits: v.array(
+      v.object({
+        userId: v.id("user"),
+        amount: v.number(),
+        paid: v.boolean(),
+      })
+    ),
+    groupId: v.optional(v.string()),
+    createdBy: v.id("users"),
+  })
+    .index("by_group", ["groupId"])
+    .index("by_user_and_group", ["paidByUserId", "groupId"])
+    .index("by_date", ["date"]),
+
+  groups: defineTable({
+    name: v.string(),
+    description: v.optional(v.string()),
+    createdBy: v.id("users"),
+    members: v.array(
+      v.object({
+        userId: v.id("user"),
+        role: v.string(),
+        joinedAt: v.number(),
+      })
+    ),
+  }),
+  settlements: defineTable({
+    amount: v.number(),
+    note: v.optional(v.string()),
+    date: v.number(),
+    paidByUsedId: v.id("users"),
+    receivedByUserId: v.id("users"),
+    groupId: v.optional(v.id("groups")),
+    relatedExpensesIds: v.optional(v.array(v.id("expenses"))),
+    createdBy: v.id("users"),
+  })
+    .index("by_group", ["groupId"])
+    .index("by_user_and_group", ["paidByUsedId", "groupId"])
+    .index("by_receiver_and_group", ["receivedByUserId", "groupId"])
+    .index("by_date", ["date"]),
 });
