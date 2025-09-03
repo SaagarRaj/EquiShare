414f3e3a1dfadb4921b035cc429fb9a532dea95b
SaagarRaj <saagartiwari1992@gmail.com>
2025-09-03 13:22:32 -0700

    group-detail-page

--- app/(main)/groups/[id]/page.jsx
+++ app/(main)/groups/[id]/page.jsx
@@ -1,5 +1,147 @@
+"use client";
+import ExpenseList from "@/components/expense-list";
+import { GroupBalances } from "@/components/group-balances";
+
+import GroupMembers from "@/components/group-members";
+import SettlementsList from "@/components/settlements-list";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { api } from "@/convex/_generated/api";
+import { useConvexQueries } from "@/hooks/use-convex-queries";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { ArrowLeft, ArrowLeftRight, PlusCircle, Users } from "lucide-react";
+import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { BarLoader } from "react-spinners";
+
 const GroupsPage = () => {
-  return <div>GroupsPage</div>;
+  const params = useParams();
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState("expense");
+
+  const { data, isLoading } = useConvexQueries(api.groups.getGroupExpenses, {
+    groupId: params.id,
+  });
+
+  //console.log(data);
+  const group = data?.group;
+  const members = data?.members || [];
+  const expenses = data?.expenses || [];
+  const settlements = data?.settlements || [];
+  const balances = data?.balances || [];
+  const userLookupMap = data?.userLookupMap || {};
+
+  if (isLoading) {
+    return (
+      <div className="container mx-auto py-12">
+        <BarLoader width={"100%"} />
+      </div>
+    );
+  }
+  return (
+    <div className="container mx-auto py-6 max-w-4xl">
+      <div className="mb-6">
+        <Button
+          className={"mb-4"}
+          variant={"outline"}
+          size="sm"
+          onClick={() => router.back()}
+        >
+          <ArrowLeft className="h-4 w-4 mr-2" /> Back
+        </Button>
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <div className="bg-primary/10 p-4 rounded-md">
+              <Users className="h-8 w-8 text-primary" />
+            </div>
+
+            <div>
+              <h1 className="text-4xl gradient-title">{group?.name}</h1>
+              <p className="text-muted-foreground">{group?.description}</p>
+              <p className="text-sm text-muted-foreground mt-1">
+                {members.length} members
+              </p>
+            </div>
+          </div>
+
+          <div className="flex gap-2 ">
+            <Button asChild variant={"outline"}>
+              <Link href={`/settlements/user/${params.id}`}>
+                <ArrowLeftRight className="mr-2 h-4 w-4 " />
+                Settle Up
+              </Link>
+            </Button>
+            <Button asChild>
+              <Link href={`/expenses/new`}>
+                <PlusCircle className="mr-2 h-4 w-4" />
+                Add Expenses
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
+        <div className="lg:col-span-2 ">
+          <Card>
+            <CardHeader className={"pb-2"}>
+              <CardTitle className={"text-xl"}>Group Balances</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <GroupBalances balances={balances} />
+            </CardContent>
+          </Card>
+        </div>
+
+        <div>
+          <Card>
+            <CardHeader className={"pb-2"}>
+              <CardTitle className={"text-xl"}>Group members</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <GroupMembers members={members} />
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+
+      <Tabs
+        defaultValue="expenses"
+        value={activeTab}
+        onValueChange={setActiveTab}
+        className="space-y-4"
+      >
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="expenses">
+            Expenses ({expenses.length})
+          </TabsTrigger>
+          <TabsTrigger value="settlements">
+            Settlements ({settlements.length})
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="expenses" className="space-y-4">
+          <ExpenseList
+            expenses={expenses}
+            showOtherPerson={true}
+            isGroupExpense={true}
+            userLookupMap={userLookupMap}
+          />
+        </TabsContent>
+
+        <TabsContent value="settlements" className="space-y-4">
+          <SettlementsList
+            settlements={settlements}
+            userLookupMap={userLookupMap}
+            isGroupSettlement={true}
+          />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default GroupsPage;
---
+++ convex/groupsSRT.js
@@ -0,0 +1,162 @@
+import { internal } from "./_generated/api";
+import { query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const getGroupExpenses = query({
+  args: { groupId: v.id("groups") },
+  handler: async (ctx, { groupId }) => {
+    const currentUser = await ctx.runQuery(internal.users.getCurrentUser);
+    const group = await ctx.db.get(groupId);
+    if (!group) throw new Error("Group not found");
+
+    if (!group.members.some((m) => m.userId === currentUser._id)) {
+      throw new Error("You are not a member of this group");
+    }
+
+    const expenses = await ctx.db
+      .query("expenses")
+      .withIndex("by_group", (q) => q.eq("groupId", groupId))
+      .collect();
+
+    const settlements = await ctx.db
+      .query("settlements")
+      .withIndex("by_group", (q) => q.eq("groupId", groupId))
+      .collect();
+
+    /* Member Detail */
+
+    const memberDetail = await Promise.all(
+      group.members.map(async (m) => {
+        const u = await ctx.db.get(m.userId);
+        return {
+          id: u._id,
+          name: u.name,
+          imageUrl: u.imageUrl,
+          role: m.role,
+        };
+      })
+    );
+    const ids = memberDetail.map((m) => m.id);
+    /*
+   Balance Calculation 
+   Initialize total object to tract overall balance for each user
+   Format: {userId1: balance1, userId2: balance2, .......}
+   */
+
+    const totals = Object.fromEntries(ids.map((id) => [id, 0]));
+
+    /*
+    Create 2-D ledger to track who owes whom 
+    ledger[A][B] = how much A owes B
+    ledger = {
+    "user1": {"user2" : 0, "user3": 0},
+    "user2": {"user1" : 0, "user3": 0},
+    "user3": {"user1" : 0, "user2": 0},
+    }
+    */
+
+    const ledger = {};
+
+    ids.forEach((a) => {
+      ledger[a] = {};
+      ids.forEach((b) => {
+        if (a != b) ledger[a][b] = 0;
+      });
+    });
+
+    /* 
+    Apply Expenses to Balances
+
+    Example: 
+    Exp1 -> user1 paid $60, split equally among all 3 users ( $20 each )
+    After applying this expense:
+    totals : {"user1" : +40 , "user2" : -20 , "user3" : -20}
+    ledger: {
+        "user1": {"user2": 0 , "user3" : 0} ,
+        "user2": {"user1": 20 , "user3" : 0} ,
+        "user3": {"user1": 20 , "user2" : 0} ,
+    }
+    Above means user2 owes $20 to user1 and user3 owes $20 to user1
+    */
+
+    for (const exp of expenses) {
+      const payer = exp.paidByUserId;
+      for (const split of exp.splits) {
+        // Skip if this is payers own split or is already paid
+        if (split.userId === payer || split.paid) continue;
+        const debtor = split.userId;
+        const amt = split.amount;
+        totals[payer] += amt;
+        totals[debtor] -= amt;
+
+        // Update totals
+
+        ledger[debtor][payer] += amt;
+      }
+    }
+
+    /* 
+    Apply Settlement to Balances
+
+    Example: 
+    User2 paid $10 to user1
+
+    After applying this settlement:
+    totals : {"user1" : +30 , "user2" : -10 , "user3" : -20}
+    ledger: {
+        "user1": {"user2": 0 , "user3" : 0} ,
+        "user2": {"user1": 10 , "user3" : 0} ,
+        "user3": {"user1": 20 , "user2" : 0} ,
+    }
+    Above means user2 owes only $10 to user1 and user3 owes $20 to user1.
+    */
+
+    for (const s of settlements) {
+      // Update totals: increase payers balance, decrease receivers balance
+      totals[s.paidByUserId] += s.amount;
+      totals[s.receivedByUserId] -= s.amount;
+
+      //Update Ledger: reduce what the payer owes to the receiver;
+      ledger[s.paidByUserId][s.receivedByUserId] -= s.amount;
+    }
+
+    /*
+
+     Format Response Data 
+     Create a comprehensive balance object for each member
+     
+     */
+
+    const balances = memberDetail.map((m) => ({
+      ...m,
+      totalBalance: totals[m.id],
+      owes: Object.entries(ledger[m.id])
+        .filter(([, v]) => v > 0)
+        .map(([to, amount]) => {
+          to, amount;
+        }),
+      owedBy: ids
+        .filter((other) => ledger[other][m.id] > 0)
+        .map((other) => ({ from: other, amount: ledger[other][m.id] })),
+    }));
+
+    const userLookUp = {};
+    memberDetail.forEach((member) => {
+      userLookUp[member.id] = member;
+    });
+
+    return {
+      // Group Information
+      group: {
+        id: group._id,
+        name: group.name,
+        description: group.description,
+      },
+      members: memberDetail, // All group member detail
+      expenses, //All Expenses in this group
+      settlements, // All settlements in this group
+      balances, // calculated balance info for each member
+      userLookUp, // Quick lookup for user detail
+    };
+  },
+});
